3c4a1b4b4144892d1de676187d4de08d1edc7200
Matt Way <matt__way@hotmail.com>
2019-08-27 08:19:43 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# block-template
+
+This is an empty Syntropy Studio block template repository for quick block repo creation.
---
+++ code.js
@@ -0,0 +1,27 @@
+/**
+ * This function runs once on start and again every time it is changed.
+ * @param {Object} args - An object containing the following
+ * @param {Object} args.state - The local state you can write to
+ * @param {HTMLDivElement} args.element - A domElement you can render to
+ * @param {Object} args.events - A set of dom event helper functions
+ * @param {number} args.iteration - The current run time iteration
+ */
+export const run = ({ state, element, events, iteration }) => {
+  // your code here
+}
+
+/**
+ * This function runs every iteration.
+ * Uncomment if you want to use it
+ * (if it exists, your program will loop continuously)
+ * @param {Object} args - An object containing the following
+ * @param {Object} args.state - The local state you can write to
+ * @param {HTMLDivElement} args.element - A domElement you can render to
+ * @param {Object} args.events - A set of dom event helper functions
+ * @param {number} args.iteration - The current run time iteration
+ */
+/*
+export const update = ({ state, element, events, iteration }) => {
+  // your code here
+}
+*/
